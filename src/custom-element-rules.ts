d01f25896679196c03d594edd3042156e3aa8f09
sem0ark <semenov0am@gmail.com>
2026-09-10 22:14:39 +0200

    chore: quick push 2026-09-10 22:14:39

--- src/custom-element-rules.ts
+++ src/custom-element-rules.ts
@@ -13,4 +13,19 @@ export const customElementRules: CustomElementRule[] = [
       return placeholder ? `\`${placeholder}\`` : "";
     },
   },
+  {
+    urlWildcard: "https://elearn.*/*",
+    cssSelector: "span.dm2-hl-link",
+    transform: (element) => {
+      const textElement = element.querySelector("div.dm2-hl-text");
+      const textContent = textElement ? textElement.textContent : "";
+
+      const directContent = Array.from(element.childNodes)
+        .filter((node) => node.nodeType === Node.TEXT_NODE)
+        .map((node) => node.textContent)
+        .join("").trim();
+
+      return `${directContent} (\`${textContent}\`)`
+    },
+  },
 ];
